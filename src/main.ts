d2d968772f77ae1880fd770da3acb8b5574bce50
Szymon Fialkowski <szymon_fialkowski@interia.pl>
2022-01-10 12:35:09 +0100

    add: yarn & node version 14

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
     dotenv.config();
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    await app.listen(port);
 }
 
 bootstrap();
